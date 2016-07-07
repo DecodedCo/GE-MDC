7c8454bd4398ebd73568796ceb5d20c1ce200379
Amadeus Stevenson <amadeus@decoded.us>
2016-07-07 19:50:50 -0400

    Working epoch version

--- app.js
+++ app.js
@@ -6,9 +6,13 @@
 
 */
 
+// Which Edge Device and sensor are we reading?
+var device = 'xlp-trainer-01';
+var desiredSensor = 'RotaryAngle';
+
 // The gateway uses Stomp for websocket streams
 var gateway = 'https://predix-isk-gateway-iskdev.run.aws-usw02-pr.ice.predix.io/stomp',
-  topic     = '/topic/xlp-trainer-01',
+  topic     = '/topic/' + device,
   ws = new SockJS(gateway),
   client = Stomp.over(ws);
 
@@ -27,65 +31,58 @@ client.connect(headers, function() {
   console.log(`Connected to ${gateway}`);
   client.subscribe(topic, processStream);
 }, function() {
-  alert("Websocket connection failed :(");
+  alert('Websocket connection failed. Try reloading the page.');
 });
 
 // Process incoming data
 function processStream(payload) {
   // get array of sensors from gateway
   var data = JSON.parse(payload.body).body;
-  // gateway returns arrays of 4 sensors at a time
+  // gateway returns arrays of 5 sensors at a time
   data.forEach(function (sensor) {
     var deviceName = sensor.name;
     var timestamp = sensor.datapoints[0][0];
     var value = sensor.datapoints[0][1];
-    var device = "xlp-trainer-01";
-    var re = new RegExp("-" + device + "$");
+    var re = new RegExp(`-${device}$`);
 
-    //console.log("%s %s", new Date(timestamp), deviceName + ": " + value);
+    //console.log(Date(timestamp), `${deviceName}: ${value}`);
 
     // only match on required sensor
     if (re.test(deviceName)) {
-      var sensorName = deviceName.replace(re, "");
-      console.log("%s %s", new Date(timestamp), sensorName + ": " + value);
-      switch (sensorName) {
-        case "Light":
+      var sensorName = deviceName.replace(re, '');
+      /*switch (sensorName) {
+        case 'Light':
           sensorSets[0].append(Date(timestamp), value);
           break;
-        case "Temperature":
+        case 'Temperature':
           sensorSets[1].append(Date(timestamp), value);
           break;
-        case "Humidity":
+        case 'Humidity':
           sensorSets[2].append(Date(timestamp), value);
           break;
-        case "RotaryAngle":
+        case 'RotaryAngle':
           sensorSets[3].append(Date(timestamp), value);
           break;
-        case "Button":
+        case 'Button':
           sensorSets[4].append(Date(timestamp), value);
           break;
-      } // end switch on sensor type
+      } // end switch on sensor type*/
+      if (sensorName == desiredSensor) {
+        myChart.push([{time: timestamp/100, y: value}]);
+        console.log(Date(timestamp), `${sensorName}: ${value}`);
+      }
     } //
-
   });
 }
 
-var seriesOptions = [
-  { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(255, 255, 0, 1)', fillStyle: 'rgba(255, 255, 0, 0.1)', lineWidth: 3 },
-  { strokeStyle: 'rgba(255, 255, 255, 1)', fillStyle: 'rgba(255, 255, 255, 0.1)', lineWidth: 3 }
-];
-
-// Initialize an empty TimeSeries for each sensor.
-var sensorSets = [new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries()]; // 5 sensors
-
-// Build the timeline
-var timeline = new SmoothieChart({ millisPerPixel: 20, grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: 1000, verticalSections: 4 }});
-
-for (var i = 0; i < sensorSets.length; i++) {
-  timeline.addTimeSeries(sensorSets[i], seriesOptions[i]);
-}
-
-timeline.streamTo(document.getElementById("sensors"), 5000); // 5 s gap
+var myChart = $('#myChart').epoch({
+  type: 'time.line',
+  data: [{
+    label: 'Rotary',
+    values:[]
+  }],
+  margins: {right: 30, left: 10},
+  ticks: {time: 5},
+  range: [0, 300],
+  axes: ['bottom', 'right', 'left']
+});
